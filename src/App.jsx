b833be5f9926fbe42555c554fd0d47f9697db3f9
Nikita <nikitapashinsky@gmail.com>
2023-01-20 19:15:16 +0300

    configure tailwind and eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="flex h-screen max-w-lg flex-col items-start justify-end gap-4 p-12 text-neutral-900">
+      <h1 className="text-4xl font-bold tracking-tight">Vite × Tailwind CSS</h1>
+      <p className="text-xl leading-8">
+        This is a starter Vite template configured with React, Javascript,
+        Tailwind CSS, and eslint.
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
